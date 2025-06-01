6f10774259666f76880f01cba6b991f18913943a
Comp <Comp@DESKTOP-GBS64RL>
2025-06-01 17:01:03 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Tracing;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Menu : Form
+    {
+
+        public static Menu instance;
+        
+        public Menu()
+        {
+            InitializeComponent();
+            instance = this;
+            
+        }
+
+        private void GameStartBtn_Click(object sender, EventArgs e)
+        {
+
+
+            if (Form1.words.Length == 1) { label2.Visible = true; }
+
+            else
+            {
+                Form1 gamewindow = new Form1();
+
+                this.Hide();
+                gamewindow.ShowDialog();
+
+                this.Show();
+            }
+        }
+           
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ChangeLangBtn_Click(object sender, EventArgs e)
+        {
+            LanguageChoice language = new LanguageChoice();
+            language.Show();
+        }
+    }
+}
